8643fdf132e580498aac70ddb8d7638436aa5a2d
Maxi580 <maxieichenseher@gmail.com>
2026-04-09 17:11:27 +0200

    langraph test

---
+++ test-agent/graph.py
@@ -0,0 +1,36 @@
+from langgraph.graph import StateGraph, START, END
+from langchain_openai import ChatOpenAI
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage, SystemMessage
+from dotenv import load_dotenv
+import operator
+import os
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: Annotated[list[BaseMessage], operator.add]
+
+
+llm = ChatOpenAI(
+    model=os.environ.get("MODEL_NAME"),
+    base_url=os.environ.get("LITELLM_BASE_URL",),
+    api_key=os.environ.get("LITELLM_API_KEY"),
+)
+
+
+def agent(state: AgentState) -> AgentState:
+    system = SystemMessage(content="You are a helpful assistant that ...")
+    response = llm.invoke([system] + state["messages"])
+    return {"messages": [response]}
+
+
+def build_graph():
+    graph = StateGraph(AgentState)
+    graph.add_node("agent", agent)
+    graph.add_edge(START, "agent")
+    graph.add_edge("agent", END)
+    return graph.compile()
+
+
+app = build_graph()
